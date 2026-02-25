fbe4a8c675a9bfc1abe82e7c547baf15d3c10a79
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-02-25 01:15:44 -0800

    Update mash_menu.lua

--- client/menus/mash_menu.lua
+++ client/menus/mash_menu.lua
@@ -34,7 +34,7 @@ local SaloonsMashMenu = FeatherMenu:RegisterMenu('saloons:mash:menu', {
     }
 )
 
-function OpenMashMenu(id, stage, currentbrew, isbrewing)
+function OpenMashMenu(id, stage, currentbrew, isbrewing, startupItem)
     -- Main list page
     local MainPage = SaloonsMashMenu:RegisterPage('main:page')
 
@@ -52,6 +52,147 @@ function OpenMashMenu(id, stage, currentbrew, isbrewing)
 
     MainPage:RegisterElement('line', { slot = "header", style = {} })
 
+    local function BuildAndOpenDetail(item, id, stage, currentbrew, isbrewing)
+        local itemCfg = Mash[item]
+        if not itemCfg then return end
+        local DetailPage = SaloonsMashMenu:RegisterPage('detail:page:' .. item)
+
+        DetailPage:RegisterElement('header', {
+            value = locales.t('MashHeader'),
+            slot = "header",
+            style = { ['color'] = '#999' }
+        })
+
+        DetailPage:RegisterElement('subheader', {
+            value = itemCfg.label,
+            slot = "header",
+            style = { ['font-size'] = '0.94vw', ['color'] = '#CC9900' }
+        })
+
+        DetailPage:RegisterElement('line', { slot = "header", style = {} })
+
+        local last = itemCfg.LastStage or 1
+        for si = 1, last do
+            local scfg = (itemCfg[si] or {})
+            local stime = scfg.fermenttime or ''
+            local ingr = scfg.ingredients or {}
+            DetailPage:RegisterElement('textdisplay', {
+                value = 'Stage ' .. tostring(si) .. ' - Time: ' .. tostring(stime) .. ' min',
+                slot = "content",
+                style = {
+                    ['font-size'] = '0.90vw',
+                    ['font-variant'] = 'small-caps',
+                    ['color'] = '#D0D0D0'
+                }
+            })
+            if ingr and type(ingr) == 'table' and #ingr > 0 then
+                DetailPage:RegisterElement('textdisplay', {
+                    value = '  Ingredients:',
+                    slot = "content",
+                    style = {
+                        ['font-size'] = '0.83vw',
+                        ['font-variant'] = 'small-caps',
+                        ['color'] = '#BFBFBF'
+                    }
+                })
+                for _, ing in ipairs(ingr) do
+                    DetailPage:RegisterElement('textdisplay', {
+                        value = '    - ' .. (ing.label or ing.id) .. ' x' .. tostring(ing.qty or 1),
+                        slot = "content",
+                        style = {
+                            ['font-size'] = '0.83vw',
+                            ['font-variant'] = 'small-caps',
+                            ['color'] = '#BFBFBF'
+                        }
+                    })
+                end
+            end
+        end
+
+        DetailPage:RegisterElement('textdisplay', {
+            value = locales.t('MashProduced') .. ' ' .. tostring(itemCfg.Yield or 1) .. ' ' .. locales.t('Buckets'),
+            slot = "content",
+            style = {
+                ['font-size'] = '0.90vw',
+                ['font-variant'] = 'small-caps',
+                ['color'] = '#BFBFBF'
+            }
+        })
+
+        -- Dynamic action button: Start / Continue / Collect
+        do
+            local disabled = (tonumber(isbrewing) or 0) == 1
+            local actionLabel = locales.t('Start') .. ' ' .. itemCfg.label
+            local actionFunc = function()
+                funcs.CallServerAsync('bcc-saloons:CheckIngredients', id, nil, item)
+            end
+
+            if currentbrew and currentbrew == item and stage and tonumber(stage) > 1 then
+                local curStage = tonumber(stage)
+                if curStage < (itemCfg.LastStage or last) then
+                    actionLabel = locales.t('ContinueBrew') .. ' ' .. itemCfg.label
+                    actionFunc = function()
+                        funcs.CallServerAsync('bcc-saloons:CheckIngredients', id, curStage, item)
+                    end
+                else
+                    actionLabel = locales.t('CollectBrew') .. ' ' .. itemCfg.label
+                    actionFunc = function()
+                        funcs.CallServerAsync('bcc-saloons:FinishBrewing', id, item, Mash)
+                    end
+                end
+            end
+
+            DetailPage:RegisterElement('button', {
+                label = actionLabel,
+                slot = "content",
+                style = { ['color'] = '#E0E0E0' },
+                disabled = disabled
+            }, function()
+                if disabled then return end
+                actionFunc()
+            end)
+        end
+
+        -- Reset mash / destroy option when brewing
+        if tonumber(isbrewing) == 1 then
+            DetailPage:RegisterElement('button', {
+                label = locales.t('DestroyMash'),
+                slot = "content",
+                style = { ['color'] = '#FF6666' }
+            }, function()
+                funcs.CallServerAsync('bcc-saloons:ResetMash', id)
+            end)
+        end
+
+        DetailPage:RegisterElement('bottomline', {
+            slot = 'footer',
+            style = {}
+        })
+
+        DetailPage:RegisterElement('button', {
+            label = locales.t('Back'),
+            slot = "footer",
+            style = { ['color'] = '#CCCCCC' }
+        }, function()
+            SaloonsMashMenu:Open({ startupPage = MainPage })
+        end)
+
+        DetailPage:RegisterElement('button', {
+            label = locales.t('Close'),
+            slot = "footer",
+            style = { ['color'] = '#CCCCCC' }
+        }, function()
+            SaloonsMashMenu:Close()
+        end)
+
+        -- show contextual tip if this mash is between stages and not currently brewing
+        if id and currentbrew and currentbrew == item and tonumber(stage) and tonumber(stage) > 1 and tonumber(isbrewing) == 0 then
+            pcall(function() TipBottom(locales.t('ContinueBrew'), 4000) end)
+        end
+
+        SaloonsMashMenu:Open({ startupPage = DetailPage })
+    end
+
     for item, itemCfg in pairs(Mash) do
         local disabled = (tonumber(isbrewing) or 0) == 1
         -- Button opens a detail page for this mash
@@ -62,139 +203,7 @@ function OpenMashMenu(id, stage, currentbrew, isbrewing)
             disabled = disabled
         }, function()
             if disabled then return end
-            -- build detail page for this mash
-            local DetailPage = SaloonsMashMenu:RegisterPage('detail:page:' .. item)
-
-            DetailPage:RegisterElement('header', {
-                value = locales.t('MashHeader'),
-                slot = "header",
-                style = { ['color'] = '#999' }
-            })
-
-            DetailPage:RegisterElement('subheader', {
-                value = itemCfg.label,
-                slot = "header",
-                style = { ['font-size'] = '0.94vw', ['color'] = '#CC9900' }
-            })
-
-            DetailPage:RegisterElement('line', { slot = "header", style = {} })
-
-            -- Stages
-            local last = itemCfg.LastStage or 1
-            for si = 1, last do
-                local scfg = (itemCfg[si] or {})
-                local stime = scfg.fermenttime or ''
-                local ingr = scfg.ingredients or {}
-                DetailPage:RegisterElement('textdisplay', {
-                    value = 'Stage ' .. tostring(si) .. ' - Time: ' .. tostring(stime) .. ' min',
-                    slot = "content",
-                    style = {
-                        ['font-size'] = '0.90vw',
-                        ['font-variant'] = 'small-caps',
-                        ['color'] = '#D0D0D0'
-                    }
-                })
-                if ingr and type(ingr) == 'table' and #ingr > 0 then
-                    DetailPage:RegisterElement('textdisplay', {
-                        value = '  Ingredients:',
-                        slot = "content",
-                        style = {
-                            ['font-size'] = '0.83vw',
-                            ['font-variant'] = 'small-caps',
-                            ['color'] = '#BFBFBF'
-                        }
-                    })
-                    for _, ing in ipairs(ingr) do
-                        DetailPage:RegisterElement('textdisplay', {
-                            value = '    - ' .. (ing.label or ing.id) .. ' x' .. tostring(ing.qty or 1),
-                            slot = "content",
-                            style = {
-                                ['font-size'] = '0.83vw',
-                                ['font-variant'] = 'small-caps',
-                                ['color'] = '#BFBFBF'
-                            }
-                        })
-                    end
-                end
-            end
-
-            DetailPage:RegisterElement('textdisplay', {
-                value = locales.t('MashProduced') .. ' ' .. tostring(itemCfg.Yield or 1) .. ' ' .. locales.t('Buckets'),
-                slot = "content",
-                style = {
-                    ['font-size'] = '0.90vw',
-                    ['font-variant'] = 'small-caps',
-                    ['color'] = '#BFBFBF'
-                }
-            })
-
-            -- Start / Continue brew button
-            local btnLabel = locales.t('Start') .. ' ' .. itemCfg.label
-            if id and currentbrew and currentbrew == item and tonumber(stage) and tonumber(stage) > 1 and tonumber(isbrewing) == 0 then
-                btnLabel = locales.t('ContinueBrew') .. ' ' .. itemCfg.label
-            end
-            DetailPage:RegisterElement('button', {
-                label = btnLabel,
-                slot = "content",
-                style = { ['color'] = '#E0E0E0' },
-                disabled = (tonumber(isbrewing) or 0) == 1
-            }, function()
-                DBG:Info('Starting/continuing brew of ' .. item .. ' with id ' .. tostring(id) .. ' stage=' .. tostring(stage))
-                funcs.CallServerAsync('bcc-saloons:CheckIngredients', id, stage, item)
-            end)
-
-            -- Collect if at last stage
-            if currentbrew and currentbrew == item and stage == (itemCfg.LastStage or 1) then
-                DetailPage:RegisterElement('button', {
-                    label = locales.t('CollectBrew') .. ' ' .. itemCfg.label,
-                    slot = "content",
-                    style = { ['color'] = '#E0E0E0' }
-                }, function()
-                    DBG:Info('Collecting brew of ' .. item .. ' with id ' .. tostring(id))
-                    funcs.CallServerAsync('bcc-saloons:FinishBrewing', id, item, Mash)
-                end)
-            end
-
-            -- Reset / Destroy if brewing
-            if tonumber(isbrewing) == 1 then
-                DetailPage:RegisterElement('button', {
-                    label = locales.t('DestroyMash'),
-                    slot = "content",
-                    style = { ['color'] = '#FF6666' }
-                }, function()
-                    DBG:Info('Destroying mash of ' .. item .. ' with id ' .. tostring(id))
-                    funcs.CallServerAsync('bcc-saloons:ResetMash', id)
-                end)
-            end
-
-            DetailPage:RegisterElement('bottomline', {
-                slot = 'footer',
-                style = {}
-            })
-
-            -- Back and Close
-            DetailPage:RegisterElement('button', {
-                label = locales.t('Back'),
-                slot = "footer",
-                style = { ['color'] = '#CCCCCC' }
-            }, function()
-                SaloonsMashMenu:Open({ startupPage = MainPage })
-            end)
-
-            DetailPage:RegisterElement('button', {
-                label = locales.t('Close'),
-                slot = "footer",
-                style = { ['color'] = '#CCCCCC' }
-            }, function()
-                SaloonsMashMenu:Close()
-            end)
-
-            -- show contextual tip if this mash is between stages and not currently brewing
-            if id and currentbrew and currentbrew == item and tonumber(stage) and tonumber(stage) > 1 and tonumber(isbrewing) == 0 then
-                pcall(function() TipBottom(locales.t('ContinueBrew'), 4000) end)
-            end
-            -- open detail page
-            SaloonsMashMenu:Open({ startupPage = DetailPage })
+            BuildAndOpenDetail(item, id, stage, currentbrew, isbrewing)
         end)
     end
 
@@ -211,6 +220,12 @@ function OpenMashMenu(id, stage, currentbrew, isbrewing)
     }, function()
         SaloonsMashMenu:Close()
     end)
+
+    -- If a startupItem was provided, try to open its detail page directly
+    if startupItem then
+        BuildAndOpenDetail(startupItem, id, stage, currentbrew, isbrewing)
+        return
+    end
 
     SaloonsMashMenu:Open({ startupPage = MainPage })
 end
